794df509268b11a7651bcb94ce99b12da17b4397
Jonathan <KuugJonathan45@gmail.com>
2022-10-01 22:06:41 +0000

    class refactor

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         String userPath = Paths.get("").toAbsolutePath().toString();
         try{
-            FlightPath fp = new FlightPath(userPath + "/src/main/accra-takoradi.txt");
+            FlightPath fp = new FlightPath(userPath + "/src/main/dubai-riyadh.txt");
             HashMap<String, Airport> airports = fp.getSourceAndDestinationAirportMeta();
             Stack<Node> path = fp.getOptimalFlightPath(airports.get("source"), airports.get("destination")).getSolutionPath();
             fp.outputDataToFile(path);
